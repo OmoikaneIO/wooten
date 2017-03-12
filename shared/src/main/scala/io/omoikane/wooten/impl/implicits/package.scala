1e7a2605b1c36957b0fe019c8da3f891e77234eb
Matthew Wampler-Doty <matthew.wampler.doty@gmail.com>
2017-03-12 11:03:41 -0400

    Adding inequality

--- shared/src/main/scala/io/omoikane/wooten/impl/implicits/package.scala
+++ shared/src/main/scala/io/omoikane/wooten/impl/implicits/package.scala
@@ -4,5 +4,6 @@ package object implicits {
   @SuppressWarnings(Array("org.wartremover.warts.Equals"))
   implicit final class AnyOps[A](self: A) {
     def ===(other: A): Boolean = self == other
+    def !==(other: A): Boolean = !(self === other)
   }
 }
